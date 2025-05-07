6d4469db7446de7f978b0042cdae7169cb7a8c75
Caesar <zoumenglinzz@sina.com>
2025-05-07 15:44:26 +1200

    2

---
+++ ML/Transpose of a Matrix.py
@@ -0,0 +1,22 @@
+# Write a Python function that computes the transpose of a given matrix.
+
+# Example:
+# Input:
+# a = [[1,2,3],[4,5,6]]
+# Output:
+# [[1,4],[2,5],[3,6]]
+# Reasoning:
+# The transpose of a matrix is obtained by flipping rows and columns.
+
+def transpose_matrix(a: list[list[int|float]]) -> list[list[int|float]]:
+	
+    
+	return b
+
+
+#tensor.t() 2-->2 dimension
+#tensor.transpose(dim0,dim1) 
+#permute(*dims)这个可以一次性重排所有维度顺序 高维度
+
+#sum(len(row) for row in a) 元素总数
+#
