b3fb8fb81df6ff460864ac6a812bb28e7cc6ae3e
Caesar <zoumenglinzz@sina.com>
2025-08-18 00:53:56 +0800

    u

--- leetcode hot100/160.py
+++ leetcode hot100/160.py
@@ -105,6 +105,36 @@ class Solution(object):
             
             
 
+#换头法：
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        A=headA
+        B=headB 
+        while A is not B:
+            if A:
+                A=A.next
+            else:
+                A=headB 
+            if B:
+                B=B.next
+            else:
+                B=headA
+        return A
 
+    
+            
+
+
+        
+        
         
         
