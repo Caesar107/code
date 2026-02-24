3ad6a599f4399af3c1efa51b1ecf7dda443d52d7
Menglin Zou <106566809+Caesar107@users.noreply.github.com>
2026-02-24 10:27:06 +0800

    Create 128.py

---
+++ leetcode_hot100/128.py
@@ -0,0 +1,54 @@
+import os
+import sys  
+'''
+128. 最长连续序列
+
+给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+
+ 
+
+示例 1：
+输入：nums = [100,4,200,1,3,2]
+输出：4
+解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+
+示例 2：
+输入：nums = [0,3,7,2,5,8,4,6,0,1]
+输出：9
+
+示例 3：
+输入：nums = [1,0,1,2]
+输出：3
+ 
+
+提示：
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+'''
+input=sys.stdin.readline().split()
+nums=list(map(int,input))
+
+
+def longestConsecutive(nums):
+    result=[1]
+    temp=1
+    sorted_nums=sorted(nums)
+    if len(sorted_nums)==0:
+        return 0
+    for i in range(len(sorted_nums)):
+        if len(sorted_nums) > i+1:
+            if sorted_nums[i+1]-sorted_nums[i]==0:
+                continue
+            if sorted_nums[i+1]-sorted_nums[i]==1:
+                temp+=1
+            else:
+                result.append(temp)
+                temp=1
+                continue
+        else:
+            result.append(temp)
+            break
+    return max(result)
+
+print(longestConsecutive(nums))
